dd10e48b710f0934e269de145ae2b0fc5717a9fa
H. Vihanga Dilshan <vihangadilshan500@gmail.com>
2025-10-16 00:18:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+// --- 1. Music Playlist ---
+const playlist = [
+    { 
+        title: "Chasing Stars", 
+        artist: "VD Vibes", 
+        file: "music/track1.mp3", 
+        // 🚨 CHANGE THIS URL to your actual image
+        cover: "images/chasing-stars-cover.jpg" 
+    },
+    { 
+        title: "Nostalgic Road", 
+        artist: "VD Vibes", 
+        file: "music/track2.mp3", 
+        // 🚨 CHANGE THIS URL to your actual image
+        cover: "images/nostalgic-road-cover.png" 
+    },
+    // ... add more songs
+];
